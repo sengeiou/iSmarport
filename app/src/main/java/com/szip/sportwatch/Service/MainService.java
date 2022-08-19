f672239f6a3231cc83bcb883e466ea25bd4d8066
sunzhipeng <szp@znsdkj.com>
2022-08-20 01:10:49 +0800

    2022.8.20

--- app/src/main/java/com/szip/sportwatch/Service/MainService.java
+++ app/src/main/java/com/szip/sportwatch/Service/MainService.java
@@ -587,13 +587,13 @@ public class MainService extends Service {
         stopNotificationService();
         mSevice = null;
         BleClient.getInstance().setiOtaResponse(null);
-        Intent intent = new Intent();
-        intent.setClass(this,MainService.class);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            startForegroundService(intent);
-        }else {
-            startService(intent);
-        }
+//        Intent intent = new Intent();
+//        intent.setClass(this,MainService.class);
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            startForegroundService(intent);
+//        }else {
+//            startService(intent);
+//        }
     }
 
 
